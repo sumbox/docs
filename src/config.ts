b9129edb03cdda1c13886650fc51fe0ea4e4b981
Rithul Kamesh <hi@rithul.dev>
2022-10-13 21:44:41 +0530

    Dos

---
+++ src/config.ts
@@ -0,0 +1,54 @@
+export const SITE = {
+	title: 'Sumbox Docs',
+	description: 'Ideas fading away is just a dream now...',
+	defaultLanguage: 'en_US',
+};
+
+export const OPEN_GRAPH = {
+	image: {
+		src: 'https://raw.githubusercontent.com/sumbox/branding/master/Logo%20Files/4.%20Icon/SVG/Icon-Color.svg',
+		alt:
+			'sumbox logo'
+	},
+	twitter: 'sumboxapp',
+};
+
+// This is the type of the frontmatter you put in the docs markdown files.
+export type Frontmatter = {
+	title: string;
+	description: string;
+	layout: string;
+	image?: { src: string; alt: string };
+	dir?: 'ltr' | 'rtl';
+	ogLocale?: string;
+	lang?: string;
+};
+
+export const KNOWN_LANGUAGES = {
+	English: 'en',
+} as const;
+export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
+
+export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
+
+export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+
+// See "Algolia" section of the README for more information.
+export const ALGOLIA = {
+	indexName: 'XXXXXXXXXX',
+	appId: 'XXXXXXXXXX',
+	apiKey: 'XXXXXXXXXX',
+};
+
+export type Sidebar = Record<
+	typeof KNOWN_LANGUAGE_CODES[number],
+	Record<string, { text: string; link: string }[]>
+>;
+export const SIDEBAR: Sidebar = {
+	en: {
+		'Introduction': [
+			{ text: 'Get Started', link: 'en/introduction' },
+		],
+		'Obsidian': [{ text: 'Page 4', link: 'en/page-4' }],
+	},
+};
---
+++ src/pages/en/introduction.md
@@ -0,0 +1,7 @@
+---
+title: Hello, World
+description: How to self host sumbox locally or on the cloud
+layout: ../../layouts/MainLayout.astro
+---
+
+This will be updated soon
